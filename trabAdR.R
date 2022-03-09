85c449db445615d68dfb540f8c6bbcffb52a26c2
enzofc708 <47937189+enzofc708@users.noreply.github.com>
2022-03-08 22:44:30 -0300

    Add files via upload

--- trabAdR.R
+++ trabAdR.R
@@ -7,23 +7,23 @@ arq_atl$Date = as.Date(as.character(arq_atl$Date), "%Y%m%d")
 arq_pac$Date = as.Date(as.character(arq_pac$Date), "%Y%m%d")
 
 
-#Testando as distribuições triangulares
+#Testando as distribui??es triangulares
 
 h <- hist(as.integer(format(arq_atl$Date,format="%Y")), breaks = 175, main="Histograma FuracÃµes/ano")
-hist(h$counts, freq=FALSE, breaks=20, main="Histograma nº Furacões no ano")
+hist(h$counts, freq=FALSE, breaks=20, main="Histograma n? Furac?es no ano")
 lines(seq(0,700,by=.5), dtriangle(seq(0,700,by=.5), 0, 700, 200), col="red")
 lines(seq(0,800,by=.5), dtriangle(seq(0,800,by=.5), 0, 800, 200), col="blue")
-lines(seq(0,750,by=.5), dtriangle(seq(0,750,by=.5), 0, 750, 200), col="green")  #ótimo
+lines(seq(0,750,by=.5), dtriangle(seq(0,750,by=.5), 0, 750, 200), col="green")  #?timo
 
 
 hist(arq_atl$Maximum.Wind[arq_atl$Maximum.Wind > 0], freq = FALSE, main="Histograma Max_Wind")
 lines(seq(10,150,by=.5), dtriangle(seq(10,150,by=.5),10,150,25), col="blue")
-lines(seq(10,150,by=.5), dtriangle(seq(10,150,by=.5),10,130,25), col="green")   #ótimo
+lines(seq(10,150,by=.5), dtriangle(seq(10,150,by=.5),10,130,25), col="green")   #?timo
 lines(seq(10,150,by=.5), dtriangle(seq(10,150,by=.5),10,110,25), col="red")
 
 
 hist(arq_atl$Minimum.Pressure[arq_atl$Minimum.Pressure > 0], freq = FALSE, main="Histograma Min_Press")
-lines(seq(890,1030,by=.5), dtriangle(seq(890,1030,by=.5),920,1020,1000), col="green")   #ótimo
+lines(seq(890,1030,by=.5), dtriangle(seq(890,1030,by=.5),920,1020,1000), col="green")   #?timo
 lines(seq(890,1030,by=.5), dtriangle(seq(890,1030,by=.5),900,1030,1000), col="blue")
 lines(seq(890,1030,by=.5), dtriangle(seq(890,1030,by=.5),930,1020,1000), col="red")
 
@@ -49,10 +49,18 @@ lines(seq(-150,50,by=.5), dnorm(seq(-150,50,by=.5), mean=mean(arq_atl$Longitude)
 
 geraFuracaoMonteCarlo <- function(n=1){
   maxWind <- rtriangle(n,10,130,25)
-  minPress <- rtriangle(n,920,1020, )
+  minPress <- rtriangle(n,920,1020,1000)
   latitude <- rnorm(n, mean=27.0449, sd=10.07788)
   longitude <- rnorm(n, mean=-65.68253, sd=19.68724)
   furacao <- c(mean(maxWind), mean(minPress), mean(latitude), mean(longitude))
   names(furacao) <- c("Maximum.Wind", "Minimum.Pressure", "Latitude", "Longitude")
   return(furacao)
+}
+
+# PrevisÃ£o de FuracÃµes em um perÃ­odo via TCL
+previsaoFuracoes <- function(anos = 10, iter=1){
+  mi_z <- anos * mean(h$counts)
+  sigma2_z <- anos * var(h$counts)
+  z <- rnorm(iter, mean = mi_z, sd = sqrt(sigma2_z))
+  return(mean(z))
 }
